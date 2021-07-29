82f458c7ceafc68e3c0ceb6e41ecdc54ee9f7ed2
Volsunga-Saga1260 <longinus1226@gmail.com>
2021-07-29 11:28:34 +0000

    movie.jsコメントアウト

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -1,11 +1,11 @@
-$(document).on('turbolinks:load', function() {
-  $('.movie-evaluation').html('<strong>評価：</strong>')
-  $('.movie-evaluation').raty({
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    },
-    path: '/assets/'
-  });
+// $(document).on('turbolinks:load', function() {
+//   $('.movie-evaluation').html('<strong>評価：</strong>')
+//   $('.movie-evaluation').raty({
+//     readOnly: true,
+//     score: function() {
+//       return $(this).attr('data-score');
+//     },
+//     path: '/assets/'
+//   });
 
-});
+// });
